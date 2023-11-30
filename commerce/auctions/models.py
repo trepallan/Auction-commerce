58b3cc65edb8bb5716115a73243123ccb7f7f444
alan shaksper <devalanfreitas@gmail.com>
2023-11-30 14:24:59 -0300

    config postgre/create models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+    
+    
+class Category(models.TextChoices):
+    FASHION = 'FASH', 'fashion'
+    ELECTRONICS = 'ELECT', 'electronics'
+    JEWELRY = 'JEWE', 'jewelry'
+    CLOTHING = 'CLOT','clothing'
+    BOOKS = 'BOOK', 'books'
+    TOYS = 'TOYS', 'toys'
+    HOME = 'HOME','home'
+    HEALTH = 'HEAL','health'
+    GAMES = 'GAME','games'
+    
+
+class auction(models.Model):
+    title = models.CharField(max_length=15)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctioneer")
+    category = models.CharField(max_length=50, choices=Category.choices)
+    image = models.URLField(blank=True) 
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist")
+
+class bids(models.Model):
+    bid = models.DecimalField(max_digits=6, decimal_places=2)
+    auction = models.ForeignKey(auction, on_delete=models.CASCADE, related_name="auction")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="caller")
+    
+    def __str__(self):
+        return f"{self.user} bid {self.bid} on {self.auction}"
+    
+class Comments(models.Model):
+    comment = models.TextField(max_length=100)
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auction_commenter")
+    iten = models.ForeignKey(auction, on_delete=models.CASCADE, related_name="auction_product")
+
+    def __str__(self):
+        return f"{self.user} coment {self.comment} on {self.iten}" 
+
+
+class sold(models.Model):
+    auction = models.IntegerField()
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="buyer")
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.auction} sold by {self.seller} for {self.value} to {self.buyer}"
--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,10 +88,17 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+name = os.environ.get("DB_NAME")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': name,
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
